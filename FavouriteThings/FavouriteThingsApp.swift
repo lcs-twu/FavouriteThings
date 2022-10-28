e41115bfb6d7a7d689e63b7c699e75c7e0ddc52c
Tom <twu@lcs.on.ca>
2022-10-28 12:13:19 -0400

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Tom Wu on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
