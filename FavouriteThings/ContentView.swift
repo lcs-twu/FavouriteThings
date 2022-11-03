a328e545445e2818258437ca1444a3e337d283fd
Tom <twu@lcs.on.ca>
2022-11-03 09:16:10 -0400

    group

--- FavouriteThings/ContentView.swift
+++ FavouriteThings/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            
+                Image("lakefield")
+            Image("lakefield2")
+                .resizable()
+                .scaledToFit()
+            
+            
+            Spacer()
         }
-        .padding()
+        .navigationTitle("Title")
     }
 }
 
---
+++ FavouriteThings/SecondThingView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondThingView.swift
+//  FavouriteThings
+//
+//  Created by Tom Wu on 2022-10-31.
+//
+
+import SwiftUI
+
+struct SecondThingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondThingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondThingView()
+    }
+}
